8d97cda20ceda21b8a796310ec3baf9e3dbb42f8
MannyAsh <amaan.ashiq.aa@gmail.com>
2023-10-23 11:10:30 +0100

    Add project files.

---
+++ DependencyInjection/Calculator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+    internal class Calculator : ICalculator
+    {
+        private readonly ILog _log;
+        private readonly IMessage _mess;
+
+        public Calculator() { }
+        public Calculator(ILog log, IMessage message)
+        {
+            _log = log;
+            _mess = message;
+        }
+
+
+        void ICalculator.Add(int a, int b)
+        {
+            int sum = (a + b);
+            string str = _mess.SendMessage($"{a} and {b} have been added succefully to {sum}");
+
+            _log.LogMess($"{str}");
+        }
+
+        void ICalculator.Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                _log.LogMess("Fatal : Cannot divide by zero!");
+
+
+            }
+            else
+            {
+                float div = (float)a / (float)b;
+                string str2 = _mess.SendMessage($"succefully divided qoutient is: {div}");
+                Console.WriteLine($"Divide done : {str2}");
+                _log.LogMess($"{str2}");
+            }
+
+        }
+    }
+}
+
+
+
+
---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,43 @@
+﻿using Autofac;
+
+namespace DependencyInjection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dependency Injection Example");
+            //--------- create a container ---------------
+            var containerBuilder = new ContainerBuilder();
+
+            //--------- Register services -----------------
+            containerBuilder.RegisterType<Message>().As<IMessage>().SingleInstance();
+            containerBuilder.RegisterType<Log>().As<ILog>().SingleInstance();
+            containerBuilder.RegisterType<Calculator>().As<ICalculator>().SingleInstance();
+
+            //--------- Build container -------------------
+            var container = containerBuilder.Build();
+            Console.WriteLine("container has been built");
+
+            //---------- Resolve services ------------------
+
+            var calc = container.Resolve<ICalculator>();
+
+            //------------- Start program ------------------
+
+            int a = 0, b = 0;
+            Console.Write("Enter first number : ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second number : ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+
+            calc.Add(a, b);
+            calc.Divide(a, b);
+
+            Console.WriteLine("done");
+            Console.ReadLine(); ;
+
+        }
+    }
+}
